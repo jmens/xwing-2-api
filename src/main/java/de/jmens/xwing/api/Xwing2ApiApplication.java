b3d003e481b7accdeffece75342ad8567b89e98f
Cemens von Musil <musil@sipgate.de>
2018-10-31 16:35:37 +0100

    initialize project

---
+++ src/main/java/de/jmens/xwing/api/Xwing2ApiApplication.java
@@ -0,0 +1,12 @@
+package de.jmens.xwing.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Xwing2ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Xwing2ApiApplication.class, args);
+	}
+}
